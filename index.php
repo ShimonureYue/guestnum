61747ef660d08a041a66e8e7bbbc9f64fda1f764
abel-valle <a.valle.chavez@gmail.com>
2017-02-03 12:08:11 -0600

    Se agrega función de comparación.

--- index.php
+++ index.php
@@ -2,9 +2,42 @@
 $numinput = '';
 $resultado_fake;
 
+function checkNumber($num_in , $num_me){
+	$numero = (string)$num_in;
+	$numero_interno = (string) $num_me;
+	
+	$numeros_input  = str_split ($numero,1);
+	$numeros_interno = str_split ($numero_interno,1);
+
+	//Verificando aparicion
+	$str_look="";
+	for ($i = 0;$i<4;$i++){//Recorremos todas las posiciones 
+		$clave = array_search($numeros_input[$i], $numeros_interno); //Busca dentro de un array un string dado
+		
+		if($clave !== false){ //Si aparece en algun momento cocateno la salida
+			$str_look.="X";
+		}
+	}
+
+	//Verifica posicion
+	$str_position="";
+	for ($i = 0;$i<4;$i++){
+		if( $numeros_interno[$i] == $numeros_input[$i])
+			$str_position .= "-";
+	}
+
+	//Excluimos la condicion de ganada
+	if($str_look == "XXXX" && $str_position == "----"){
+		$final_result = $str_position;
+	}else{
+		$final_result = $str_look.$str_position;
+	}
+	return $final_result;
+}
+
 if( isset($_POST['textarea']) ) {
     $numinput = $_POST['textarea'];
-    echo $numinput."<br>";
+    checkNumber($numinput, 1234);
 }
 
 function respuesta() {
@@ -51,6 +84,7 @@ function respuesta() {
                     </div>
                 </center>
             </form>
+            
         </div>
 </body>
 </html>
